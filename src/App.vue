d7f1417d813586989f41ef2216d9cd0d8304a313
Ryan Rambali <ryanrambali@gmail.com>
2025-09-21 14:55:57 -0400

    added Shadcn Vue and Tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
-<script setup></script>
+<script setup>
+import { Button } from '@/components/ui/button'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <Button>Button</Button>
 </template>
 
 <style scoped></style>
